83972e7250b80d9f374d2b256a108bc2e664f33d
Javier Aroche <aroche290@gmail.com>
2017-01-03 10:07:54 -0800

    update(descriptor-info-example): added optional params example

--- example/descriptor-info-example.jsx
+++ example/descriptor-info-example.jsx
@@ -1,15 +1,21 @@
-// Include the descriptor-info module
-#include "../jsx/descriptor-info.jsx"
 // Include the JSON helper
 #include "./helpers/JSON.jsx"
+// Include the descriptor-info module
+#include "../jsx/descriptor-info.jsx"
   
 // ActionDescriptor example  
 var ref = new ActionReference();  
 ref.putEnumerated( charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt") );  
 var desc = executeActionGet(ref);   
-  
-// Retrieve its properties by running the getProperties function, passing the ActionDescriptor as a param  
-var descObject = descriptorInfo.getProperties( desc );
+
+// Retrieve its properties by running the getProperties function, passing the ActionDescriptor as a param
+// Optional param object: descFlags
+// Optional flag boolean: reference - return reference descriptors. Could slighly affect speed.
+var descFlags = {
+    reference : false
+};
+
+var descObject = descriptorInfo.getProperties( desc ); // If using descFlags --> descriptorInfo.getProperties( desc, descFlags );
 
 // Running in ExtendScript
 $.writeln(JSON.stringify(descObject, null, 4));
