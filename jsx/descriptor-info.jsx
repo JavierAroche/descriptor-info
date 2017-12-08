eecbed68f1883f5a3d599a09056db0c1e908bb3d
Javier Aroche <aroche290@gmail.com>
2017-12-08 00:22:52 -0800

    update(): added maxRawLimit, maxXMPLimit and saveToFile params. Updated function comments

--- jsx/descriptor-info.jsx
+++ jsx/descriptor-info.jsx
@@ -1,60 +1,72 @@
- /*
- *
- * Descriptor-Info
- * JSX script to recursively get all the properties in an ActionDescriptor used in Adobe applications
- * 
- * Author: Javier Aroche (https://github.com/JavierAroche)
- * Repo: https://github.com/JavierAroche/descriptor-info
- * Version: v1.0.0
- * License MIT
- *
- */
+/**
+  *
+  * Descriptor-Info
+  * JSX script to recursively get all the properties in an ActionDescriptor used in Adobe applications
+  *
+  * Author: Javier Aroche (https://github.com/JavierAroche)
+  * Repo: https://github.com/JavierAroche/descriptor-info
+  * Version: v1.0.0
+  * License MIT
+  *
+  */
 
-/*
- * Descriptor Info constructor.
- * @constructor
- */
+/**
+  * Descriptor Info constructor.
+  * @constructor
+  */
 function DescriptorInfo() {}
 
-/*
- * @public
- * Handler function to get Action Descriptor properties
- * @param {Object} Action Descriptor
- * @param {Object} Optional params object
- *    @flag {Boolean} reference - return reference descriptors. Could slightly affect speed. Default = false.
- *    @flag {Boolean} extended - returns extended information about the descriptor. Default = false.
- *    @flag {Number} maxRawLimit - limits the max number of characters from a RAWTYPE descriptor. Default = 10000.
- */
+/**
+  * @public
+  * Handler function to get Action Descriptor properties
+  * @param {Object} Action Descriptor
+  * @param {Object} Optional params object
+  * 	@param {Boolean} reference - return reference descriptors. Could slightly affect speed. Default = false.
+  * 	@param {Boolean} extended - returns extended information about the descriptor. Default = false.
+  * 	@param {Number} maxRawLimit - limits the max number of characters from a RAWTYPE descriptor. Default = 10000.
+  * 	@param {Number} maxXMPLimit - limits the max number of characters from an XMPMetadataAsUTF8 property. Default = 10000.
+  * 	@param {String} saveToFile - Saves the descriptor to a JSON file. Default = '~/Desktop/descriptor-info.json'.
+  */
 DescriptorInfo.prototype.getProperties = function( theDesc, params ) {
 	// Define params
 	this.descParams = {
-		reference : params ? params.reference : false,
-		extended : params ? params.extended : false,
-		maxRawLimit : params ? params.maxRawLimit : 10000
+		reference : params.reference || false,
+		extended : params.extended || false,
+		maxRawLimit : params.maxRawLimit || 10000,
+		maxXMPLimit : params.maxXMPLimit || 10000,
+		saveToFile : params.saveToFile || '~/Desktop/descriptor-info.json'
 	};
 
+	var descObject;
 	if( theDesc == '[ActionList]' ) {
-		return this._getDescList( theDesc );
+		descObject = this._getDescList( theDesc );
 	} else {
-		return this._getDescObject( theDesc, {} );
+		descObject = this._getDescObject( theDesc, {} );
 	}
+
+	if(params.hasOwnProperty('saveToFile')) {
+		alert(true);
+		this._saveToFile( descObject, this.descParams.saveToFile );
+	}
+
+	return descObject;
 };
 
-/*
- * @private
- * Handler function to get the items in an ActionDescriptor Object
- * @param {Object} Action Descritor
- * @param {Object} Empty object to return (required since it's a recursive function)
- */
+/**
+  * @private
+  * Handler function to get the items in an ActionDescriptor Object
+  * @param {Object} Action Descritor
+  * @param {Object} Empty object to return (required since it's a recursive function)
+  */
 DescriptorInfo.prototype._getDescObject = function( theDesc, descObject ) {
 	for ( var i = 0; i < theDesc.count; i++ ) {
 			var typeID = theDesc.getKey(i);
 			var descType = ( theDesc.getType( typeID ) ).toString();
-			
+
 			var descProperties,
 				descStringID = typeIDToStringID( typeID ),
 				descCharID = typeIDToCharID( typeID );
-            
+
 			if( this.descParams.extended ) {
 				descProperties = {
 					stringID : descStringID,
@@ -67,9 +79,9 @@ DescriptorInfo.prototype._getDescObject = function( theDesc, descObject ) {
 			} else {
 				descProperties = this._getValue( theDesc, descType, typeID );
 			}
-            
+
 			var objectName = this._getBestName( typeID );
-            
+
 			switch( descType ) {
 				case 'DescValueType.OBJECTTYPE':
 					if( this.descParams.extended ) {
@@ -78,7 +90,7 @@ DescriptorInfo.prototype._getDescObject = function( theDesc, descObject ) {
 						descProperties = this._getDescObject( descProperties, {} );
 					}
 				break;
-                
+
 			case 'DescValueType.LISTTYPE':
 					if( this.descParams.extended ) {
                     	descProperties.list = this._getDescList( descProperties.value );
@@ -86,12 +98,12 @@ DescriptorInfo.prototype._getDescObject = function( theDesc, descObject ) {
 						descProperties = this._getDescList( descProperties );
 					}
                     break;
-                    
+
 			case 'DescValueType.ENUMERATEDTYPE':
-				descProperties.enumerationType = typeIDToStringID(theDesc.getEnumerationType( typeID ));  
+				descProperties.enumerationType = typeIDToStringID(theDesc.getEnumerationType( typeID ));
 				break;
 
-			case 'DescValueType.REFERENCETYPE': 
+			case 'DescValueType.REFERENCETYPE':
 				if( this.descParams.reference ) {
 						var referenceValue;
 
@@ -127,7 +139,7 @@ DescriptorInfo.prototype._getDescObject = function( theDesc, descObject ) {
 					}
 					break;
 
-				default: 
+				default:
 					break;
 			}
 
@@ -137,11 +149,11 @@ DescriptorInfo.prototype._getDescObject = function( theDesc, descObject ) {
 	return descObject;
 };
 
-/*
- * @private
- * Handler function to get the items in an ActionList
- * @param {Object} Action List
- */
+/**
+  * @private
+  * Handler function to get the items in an ActionList
+  * @param {Object} Action List
+  */
 DescriptorInfo.prototype._getDescList = function( list ) {
 	var listArray = [];
 
@@ -152,10 +164,10 @@ DescriptorInfo.prototype._getDescList = function( list ) {
 		switch( listItemType ) {
 			case 'DescValueType.OBJECTTYPE':
 				var listItemOBJ = {};
-				
+
 				var listItemProperties,
 					descStringID = typeIDToStringID( list.getClass(ii) );
-                
+
 				if( this.descParams.extended ) {
 					listItemProperties = {
 						stringID : descStringID,
@@ -168,7 +180,7 @@ DescriptorInfo.prototype._getDescList = function( list ) {
 				} else {
 					listItemProperties = this._getDescObject( listItemValue, {} );
 				}
-                
+
 				var listItemObject = {};
 				listItemObject[descStringID] = listItemProperties;
 
@@ -180,9 +192,9 @@ DescriptorInfo.prototype._getDescList = function( list ) {
 				break;
 
 			case 'DescValueType.REFERENCETYPE':
-				if( this.descParams.reference ) {					
+				if( this.descParams.reference ) {
 					var referenceProperties = {};
-					
+
 					try {
 						referenceProperties.actionReference = this._getActionReferenceInfo( listItemValue );
 					} catch( err) {
@@ -206,15 +218,15 @@ DescriptorInfo.prototype._getDescList = function( list ) {
 					} catch( err ) {
 						$.writeln( "Unable to run executeActionGet from container: " + descStringID + ' - ' + err );
 					}
-					
+
 					listArray.push( referenceProperties );
-					
+
 				} else {
 					listArray.push( listItemValue );
 				}
 				break;
 
-			default: 
+			default:
 				listArray.push( listItemValue );
 				break;
 		}
@@ -223,65 +235,70 @@ DescriptorInfo.prototype._getDescList = function( list ) {
 	return listArray;
 }
 
-/*
- * @private
- *
- * Based on code by Michael Hale
- * http://www.ps-scripts.com/
- *
- * Handler function to get the value of an Action Descriptor
- * @param {Object} Action Descriptor
- * @param {String} Action Descriptor type
- * @param {Number} Action Descriptor Key / Index
- */
+/**
+  * @private
+  *
+  * Based on code by Michael Hale
+  * http://www.ps-scripts.com/
+  *
+  * Handler function to get the value of an Action Descriptor
+  * @param {Object} Action Descriptor
+  * @param {String} Action Descriptor type
+  * @param {Number} Action Descriptor Key / Index
+  */
 DescriptorInfo.prototype._getValue = function( theDesc, descType, position ) {
 	switch( descType ) {
-		case 'DescValueType.BOOLEANTYPE':  
-			return theDesc.getBoolean( position );  
+		case 'DescValueType.BOOLEANTYPE':
+			return theDesc.getBoolean( position );
 			break;
 
-		case 'DescValueType.CLASSTYPE':  
-			return theDesc.getClass( position );  
+		case 'DescValueType.CLASSTYPE':
+			return theDesc.getClass( position );
 			break;
 
-		case 'DescValueType.DOUBLETYPE':  
-			return theDesc.getDouble( position );  
+		case 'DescValueType.DOUBLETYPE':
+			return theDesc.getDouble( position );
 			break;
 
-		case 'DescValueType.ENUMERATEDTYPE':  
-			return typeIDToStringID(theDesc.getEnumerationValue( position ));  
+		case 'DescValueType.ENUMERATEDTYPE':
+			return typeIDToStringID(theDesc.getEnumerationValue( position ));
 			break;
 
-		case 'DescValueType.INTEGERTYPE':  
-			return theDesc.getInteger( position );  
+		case 'DescValueType.INTEGERTYPE':
+			return theDesc.getInteger( position );
 			break;
 
-		case 'DescValueType.LISTTYPE':  
-			return theDesc.getList( position );  
+		case 'DescValueType.LISTTYPE':
+			return theDesc.getList( position );
 			break;
 
 		case 'DescValueType.OBJECTTYPE':
-			return theDesc.getObjectValue( position );  
+			return theDesc.getObjectValue( position );
 			break;
 
 		case 'DescValueType.REFERENCETYPE':
-			return theDesc.getReference( position );  
+			return theDesc.getReference( position );
 			break;
 
 		case 'DescValueType.STRINGTYPE':
 			var str = '';
-			return str + theDesc.getString( position );  
+			if( typeIDToStringID( position ) == 'XMPMetadataAsUTF8' ) {
+				return str + JSON.stringify(theDesc.getString( position )).substring( 0, this.descParams.maxXMPLimit );
+			} else {
+				return str + theDesc.getString( position );
+			}
+
 			break;
 
-		case 'DescValueType.UNITDOUBLE':  
-			return theDesc.getUnitDoubleValue( position );  
-			break;        
+		case 'DescValueType.UNITDOUBLE':
+			return theDesc.getUnitDoubleValue( position );
+			break;
 
-		case 'DescValueType.ALIASTYPE':  
+		case 'DescValueType.ALIASTYPE':
 			return decodeURI(theDesc.getPath( position ));
 			break;
 
-		case 'DescValueType.RAWTYPE':  
+		case 'DescValueType.RAWTYPE':
 			return theDesc.getData( position ).substring( 0, this.descParams.maxRawLimit );
 			break;
 
@@ -317,17 +334,16 @@ DescriptorInfo.prototype._getValue = function( theDesc, descType, position ) {
 			break;
 
 		default:
-			break;  
+			break;
 	};
 };
 
-/*
- * @private
- *
- * Handler function to get the info about action reference
- * @param {Object} Action Reference
- */
-
+/**
+  * @private
+  *
+  * Handler function to get the info about action reference
+  * @param {Object} Action Reference
+  */
 DescriptorInfo.prototype._getActionReferenceInfo = function( reference ) {
 	var form = reference.getForm().toString();
 	var classID = reference.getDesiredClass();
@@ -349,12 +365,12 @@ DescriptorInfo.prototype._getActionReferenceInfo = function( reference ) {
 }
 
 
-/*
- * @private
- *
- * Handler function to get the best name for typeID
- * @param {Number} typeID
- */
+/**
+  * @private
+  *
+  * Handler function to get the best name for typeID
+  * @param {Number} typeID
+  */
 DescriptorInfo.prototype._getBestName = function( typeID ) {
 	var stringValue = typeIDToStringID( typeID );
 	var charValue = typeIDToCharID( typeID );
@@ -366,6 +382,25 @@ DescriptorInfo.prototype._getBestName = function( typeID ) {
 	} else {
 		return typeID + "";
 	}
+}
+
+/**
+  * @private
+  *
+  * Handler function to save descriptor into a file
+  * @param {String} descriptor
+  */
+DescriptorInfo.prototype._saveToFile = function( descriptor, outputFile ) {
+	outputFile = File(outputFile);
+
+	if(outputFile.exists) {
+		outputFile.remove();
+	}
+
+	outputFile.encoding = "UTF8";
+	outputFile.open( "e", "TEXT", "????" );
+	outputFile.writeln( JSON.stringify(descriptor, null, 4) );
+	outputFile.close();
 }
 
 // Create a new Descriptor instance
