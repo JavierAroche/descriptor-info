bbae3e887d57bd76379f18dc0ee231b267fdc3d2
Javier Aroche <aroche290@gmail.com>
2017-12-08 00:23:07 -0800

    update(): updated example

--- example/descriptor-info-example.jsx
+++ example/descriptor-info-example.jsx
@@ -2,21 +2,25 @@
 #include "./helpers/JSON.jsx"
 // Include the descriptor-info module
 #include "../jsx/descriptor-info.jsx"
-  
-// ActionDescriptor example  
-var ref = new ActionReference();  
-ref.putEnumerated( charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt") );  
-var desc = executeActionGet(ref);   
 
-// Retrieve its properties by running the getProperties function, passing the ActionDescriptor as a param
-// Optional @param {Object} descFlags
-// Optional @flag {Boolean} reference - return reference descriptors. Could slightly affect speed. Default = false.
-// Optional @flag {Boolean} extended - returns extended information about the descriptor. Default = false.
-// Optional @flag {Number} maxRawLimit - limits the max number of characters from a RAWTYPE descriptor. Default = 10000.
+// ActionDescriptor example
+var ref = new ActionReference();
+ref.putEnumerated( charIDToTypeID("Dcmn"), charIDToTypeID("Ordn"), charIDToTypeID("Trgt") );
+var desc = executeActionGet(ref);
+
+/** Retrieve its properties by running the getProperties function, passing the ActionDescriptor as a param
+  * @param {Boolean} reference - return reference descriptors. Could slightly affect speed. Default = false.
+  * @param {Boolean} extended - returns extended information about the descriptor. Default = false.
+  * @param {Number} maxRawLimit - limits the max number of characters from a RAWTYPE descriptor. Default = 10000.
+  * @param {Number} maxXMPLimit - limits the max number of characters from an XMPMetadataAsUTF8 property. Default = 10000.
+  * @param {String} saveToFile - Saves the descriptor to a JSON file. Default = '~/Desktop/descriptor-info.json'.
+  */
 var descFlags = {
-    reference : false,
+	reference : false,
 	extended : false,
-	maxRawLimit : 10000
+	maxRawLimit : 10000,
+	maxXMPLimit : 100000,
+	saveToFile: '~/Desktop/descriptor-info.json'
 };
 
 var descObject = descriptorInfo.getProperties( desc, descFlags );
@@ -27,4 +31,4 @@ $.writeln(JSON.stringify(descObject, null, 4));
 // Running in Brackets with the Brackets-to-Photoshop extension
 // https://github.com/JavierAroche/brackets-to-photoshop
 // Brackets-to-Photoshop extension includes the JSON helper by default
-$.writeln(descObject);
+// $.writeln(descObject);
